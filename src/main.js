97f69700ea4fe499e95173455437bc308215f69d
skyscraper4 <jcardenas1@ucol.mx>
2020-12-03 17:35:04 -0600

    Implemented sumarParesFor

--- src/main.js
+++ src/main.js
@@ -1,3 +1,17 @@
-export default class App {}
+export default class App {
+ 
+    sumarParesFor() {
+      let suma = 0;
+        for(let x = 1; x <= 20; x = x + 1) {
+        if(x%2 == 0) {
+            suma = suma + x;
+        }
+      }  
+    return suma;
+    }
+
+}
 
 let app = new App();
+
+console.log(app.sumarParesFor());
