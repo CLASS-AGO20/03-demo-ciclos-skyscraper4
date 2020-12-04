faa2af4ba377214c8002a9c2c62f225d6cbd1b3f
skyscraper4 <jcardenas1@ucol.mx>
2020-12-03 19:47:46 -0600

    Implemented contarImparesFor

--- src/main.js
+++ src/main.js
@@ -10,8 +10,21 @@ export default class App {
     return suma;
     }
 
+    contarImparesFor(inicio, fin) {
+     let impares = 0;
+        for(let i = inicio; i <= fin; i = i + 1) {
+            if(i%2 != 0) {
+                impares = impares + 1;
+            }
+        }   
+    return impares;
+    }
+
 }
 
 let app = new App();
 
 console.log(app.sumarParesFor());
+
+console.log(app.contarImparesFor(4, 23));
+console.log(app.contarImparesFor(3, 25));
